41972e9dc71e0b2e5527735a3a84d35291e93f4a
Moymuna Jahan Sadia <jahansadia0026@gmail.com>
2026-05-07 16:10:29 +0600

    add

---
+++ jee69_angular/src/app/model/student.model.ts
@@ -0,0 +1,9 @@
+export interface StudentModel{
+
+id? :string;
+name:string;
+email:string;
+cellNo:string;
+fee:number;
+
+}
---
+++ jee69_angular/src/app/services/student.services.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentServices {
+private baseApi: string="http://localhost:4200/student";
+
+}
